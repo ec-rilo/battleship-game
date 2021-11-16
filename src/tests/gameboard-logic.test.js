cbc7cbb4a2c236cfee92f46fddc65075d8fdbaae
Edgar <ecarrillo046@gmail.com>
2021-11-15 16:21:16 -0800

    feat: add new code from ./receive-attack to test receiveAttack()

--- src/tests/gameboard-logic.test.js
+++ src/tests/gameboard-logic.test.js
@@ -1,6 +1,7 @@
 import { test1 } from './gameboard-logic';
 import { gameboard1, defaultBoardArr } from './board-population';
 import { gameboard2, populatedArr1 } from './ship-placement';
+import gameboard3 from './receive-attack';
 
 test('returns 11 rows and 11 columns in the 2d array initialized as false', () => {
   expect(gameboard1.popBoard()).toEqual(defaultBoardArr);
@@ -11,5 +12,5 @@ test('Check that the gameboard can populate a ship at 0,0 and 0,1', () => {
 });
 
 test('Populate gameboard with hit on receiveAttack()', () => {
-  expect(test1.receiveAttack()).toEqual({ populated: true, hit: true });
+  expect(gameboard3.receiveAttack()).toEqual({ populated: true, hit: true });
 });
