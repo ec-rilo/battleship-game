cf0a7d861b0a37044fe7260811db6d69c57c6204
Edgar <ecarrillo046@gmail.com>
2021-11-16 08:30:32 -0800

    docs: delete duplicate file

--- src/tests/ship-logic.test.js
+++
@@ -1,9 +0,0 @@
-import { ship1, ship2 } from './sunk-check';
-
-test('Returns false when some squares are hit', () => {
-  expect(ship1.isSunk()).toBeFalsy();
-});
-
-test('Returns true when all squares are hit', () => {
-  expect(ship2.isSunk()).toBeTruthy();
-});
