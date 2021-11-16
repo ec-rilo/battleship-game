ade2c80ae362090637d04a4e42188cf5f6765cfa
Edgar <ecarrillo046@gmail.com>
2021-11-16 12:38:10 -0800

    feat: add test to see if ship.hit() is called when receiveAttack is called

--- src/tests/Gameboard/gameboard-logic.test.js
+++ src/tests/Gameboard/gameboard-logic.test.js
@@ -1,6 +1,6 @@
 import { gameboard1, defaultBoardArr } from './board-population';
 import { gameboard2, populatedArr1 } from './ship-placement';
-import gameboard3 from './receive-attack';
+import { gameboard3, gameboard6 } from './receive-attack';
 import { gameboard4, gameboard5 } from './ship-sunk-check';
 
 test('returns 11 rows and 11 columns in the 2d array initialized as false', () => {
@@ -13,6 +13,12 @@ test('Check that the gameboard can populate a ship at 0,0 and 0,1', () => {
 
 test('Populate gameboard with hit on receiveAttack()', () => {
   expect(gameboard3.receiveAttack()).toEqual({ populated: true, hit: true });
+});
+
+// It should be marked as sunk because I added extra code marking the remaining
+// ship squares to be hit when this is called.
+test('ship should be marked true that it is sunk when hit() is called.', () => {
+  expect(gameboard6.receiveAttack()).toBeTruthy();
 });
 
 test('Reports true when all ships are sunk', () => {
