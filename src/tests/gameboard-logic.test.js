81ca4187164ecd823ffe734117b997c90f103b98
Edgar <ecarrillo046@gmail.com>
2021-11-15 17:56:13 -0800

    feat: test when all or some ships are sunk

--- src/tests/gameboard-logic.test.js
+++ src/tests/gameboard-logic.test.js
@@ -1,6 +1,7 @@
 import { gameboard1, defaultBoardArr } from './board-population';
 import { gameboard2, populatedArr1 } from './ship-placement';
 import gameboard3 from './receive-attack';
+import { gameboard4, gameboard5 } from './ship-sunk-check';
 
 test('returns 11 rows and 11 columns in the 2d array initialized as false', () => {
   expect(gameboard1.popBoard()).toEqual(defaultBoardArr);
@@ -12,4 +13,12 @@ test('Check that the gameboard can populate a ship at 0,0 and 0,1', () => {
 
 test('Populate gameboard with hit on receiveAttack()', () => {
   expect(gameboard3.receiveAttack()).toEqual({ populated: true, hit: true });
+});
+
+test('Reports true when all ships are sunk', () => {
+  expect(gameboard4.allSunk()).toBeTruthy();
+});
+
+test('Reports false when some ships are sunk', () => {
+  expect(gameboard5.allSunk()).toBeFalsy();
 });
