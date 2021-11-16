e1950c16e62e7e4464018b11336743e425cea41c
Edgar <ecarrillo046@gmail.com>
2021-11-15 16:02:39 -0800

    feat: import new code to test ship placement

--- src/tests/gameboard-logic.test.js
+++ src/tests/gameboard-logic.test.js
@@ -1,12 +1,13 @@
 import { test1, array2 } from './gameboard-logic';
 import { gameboard1, defaultBoardArr } from './board-population';
+import { gameboard2, populatedArr1 } from './ship-placement';
 
 test('returns 11 rows and 11 columns in the 2d array initialized as false', () => {
   expect(gameboard1.popBoard()).toEqual(defaultBoardArr);
 });
 
-test('Place ship on gameboard that has false values', () => {
-  expect(test1.placeShip()).toEqual(array2);
+test('Check that the gameboard can populate a ship at 0,0 and 0,1', () => {
+  expect(gameboard2.placeShip()).toEqual(populatedArr1);
 });
 
 test('Populate gameboard with hit on recieveAttack()', () => {
