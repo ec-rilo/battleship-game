1cb6d354239dbe60628323c977170c3943b2a9f7
Edgar <ecarrillo046@gmail.com>
2021-11-17 08:59:34 -0800

    docs: refactor gameboard6 test to expect if ship sunk

--- src/tests/Gameboard/gameboard-logic.test.js
+++ src/tests/Gameboard/gameboard-logic.test.js
@@ -26,7 +26,9 @@ test('Populate gameboard with hit on receiveAttack()', () => {
 // Expected ship should be marked as sunk.
 // I marked the remaining shipSquares as hit when receiveAttack() is called.
 test('ship should be marked true that it is sunk when hit() is called.', () => {
-  expect(gameboard6.receiveAttack()).toBeTruthy();
+  expect(
+    gameboard6.shipsArr[gameboard6.board[0][0].ship].isSunk()
+  ).toBeTruthy();
 });
 
 test('Reports true when all ships are sunk', () => {
