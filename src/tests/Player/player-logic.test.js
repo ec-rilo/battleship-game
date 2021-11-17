da10f299987aec9b5bd3d86e40d1c6087ccfaea2
Edgar <ecarrillo046@gmail.com>
2021-11-17 09:57:07 -0800

    feat: add computer can attack random location test

--- src/tests/Player/player-logic.test.js
+++ src/tests/Player/player-logic.test.js
@@ -1,5 +1,10 @@
 import userAttackPlayer1 from './user-attack';
-import { compAttackPlayer1, compAttackPlayer1Arr } from './comp-attack';
+import {
+  compAttackPlayer1,
+  compAttackPlayer1Arr,
+  compAttackPlayer2,
+  compAttackPlayer2Arr,
+} from './comp-attack';
 
 test('user is capable of attacking enemy computer', () => {
   expect(userAttackPlayer1.gameboard.allSunk()).toBeTruthy();
@@ -7,4 +12,8 @@ test('user is capable of attacking enemy computer', () => {
 
 test('computer can attack enemy player', () => {
   expect(compAttackPlayer1.gameboard.board).toEqual(compAttackPlayer1Arr);
+});
+
+test('computer can attack enemy player at random spots', () => {
+  expect(compAttackPlayer2.gameboard.board).toEqual(compAttackPlayer2Arr);
 });
