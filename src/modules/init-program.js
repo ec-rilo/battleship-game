71e8178edd176a85606fba92e02eeb627d480158
Edgar <ecarrillo046@gmail.com>
2021-11-19 12:28:49 -0800

    docs: refactor popMainPage() to popGBS

--- src/modules/init-program.js
+++ src/modules/init-program.js
@@ -1,15 +1,12 @@
 import { popGBSquares } from './dom-creation';
 
-const popMainPage = () => {
-  const GB1 = document.querySelector('.gameboard-1');
-  popGBSquares(GB1);
-
-  const GB2 = document.querySelector('.gameboard-2');
-  popGBSquares(GB2);
+const popGBS = () => {
+  const GB = [...document.querySelectorAll('.gameboard-interface')];
+  GB.forEach((gameboard) => popGBSquares(gameboard));
 };
 
 function initProgram() {
-  popMainPage();
+  popGBS();
 }
 
 export default initProgram;
