3a615e44772eb6846670d722987a1ad17ea73b67
Edgar <ecarrillo046@gmail.com>
2021-11-19 11:07:42 -0800

    docs: resize board to be 10x10

--- src/tests/Gameboard/gameboard.js
+++ src/tests/Gameboard/gameboard.js
@@ -3,9 +3,9 @@ import Ship from '../Ship/ship-logic';
 const Gameboard = () => {
   const board = [];
 
-  for (let i = 0; i < 11; ++i) {
+  for (let i = 0; i < 10; ++i) {
     board.push([]);
-    for (let j = 0; j < 11; ++j) {
+    for (let j = 0; j < 10; ++j) {
       board[i][j] = { populated: false, hit: false, ship: undefined };
     }
   }
