404b5ca2a3c1c70b247b5b5a42146b6d0907f688
Edgar <ecarrillo046@gmail.com>
2021-11-22 19:37:27 -0800

    feat: test that attackCoord can be returned

---
+++ src/tests/Player/comp-random-coords-check.js
@@ -0,0 +1,49 @@
+import Gameboard from '../Gameboard/gameboard';
+import Player from './player';
+
+function initShips1(player) {
+  player.gameboard.placeShip([
+    { x: 0, y: 3 },
+    { x: 0, y: 4 },
+    { x: 0, y: 5 },
+  ]);
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const CompPlayer = () => {
+  const gameboard = Gameboard();
+  let attackCoord = {};
+
+  const attack = (enemy) => {
+    let isValidCoord = false;
+
+    while (isValidCoord === false) {
+      const xCoord = 4; // Placeholder to see if attackCoord will be returned
+      const yCoord = 2; // Placeholder to see if attackCoord will be returned
+      attackCoord = { x: xCoord, y: yCoord };
+      if (enemy.gameboard.board[xCoord][yCoord].hit === false) {
+        enemy.damage(attackCoord);
+        isValidCoord = true;
+      }
+    }
+  };
+
+  function getAttackCoord() {
+    return attackCoord;
+  }
+
+  const damage = (attackCoords) => {
+    gameboard.receiveAttack(attackCoords);
+  };
+
+  return { attack, damage, gameboard, getAttackCoord };
+};
+
+const coordsCheckPlayer = Player();
+initShips1(coordsCheckPlayer);
+const coordsCheckComp = CompPlayer();
+
+export { coordsCheckComp, coordsCheckPlayer };
