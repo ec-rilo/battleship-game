82c3bda932a09164f06716aa01e5909cb81dc170
Edgar <ecarrillo046@gmail.com>
2021-11-25 11:53:52 -0800

    docs: remove createPopupStart()

--- src/modules/start-popup/popup-logic.js
+++ src/modules/start-popup/popup-logic.js
@@ -30,11 +30,12 @@ function spuGridLogic(grid, btnContainer) {
   const initEachSquare = (row, rows, rowArr) => {
     row.forEach((square) => {
       const createHorizSquareArr = (currSquare) => {
+        let horizSquare = currSquare;
         const horizSquareArr = [];
         for (let i = 0; i < popupShips[count].length; ++i) {
-          if (currSquare !== null) {
-            horizSquareArr.push(currSquare);
-            currSquare = currSquare.nextSibling;
+          if (horizSquare !== null) {
+            horizSquareArr.push(horizSquare);
+            horizSquare = horizSquare.nextSibling;
           }
         }
         return horizSquareArr;
@@ -165,83 +166,4 @@ function spuGridLogic(grid, btnContainer) {
   rowSquares.forEach((row) => initEachSquare(row, rows, rowSquares));
 }
 
-function createStartPopup() {
-  const createCard = () => {
-    const card = document.createElement('div');
-    card.classList.add('ship-placement-popup-container');
-    return card;
-  };
-
-  const createTextContainer = () => {
-    const container = document.createElement('div');
-    container.classList.add('popup-text-container');
-
-    const title = document.createElement('p');
-    title.innerHTML = 'Welcome to BATTLESHIP!';
-    container.appendChild(title);
-
-    const subTitle = document.createElement('p');
-    subTitle.innerHTML = `Place your <u>Carrier</u>`;
-    container.appendChild(subTitle);
-
-    return container;
-  };
-
-  const createBtnContainer = () => {
-    const container = document.createElement('div');
-    container.classList.add('rotate-ship-btn-container');
-
-    const xBtn = document.createElement('div');
-    xBtn.classList.add('rotate-x-btn');
-    xBtn.classList.add('rotate-btn');
-    xBtn.classList.add('rotate-btn-active');
-    xBtn.innerHTML = 'Rotate - X';
-    container.appendChild(xBtn);
-
-    const yBtn = document.createElement('div');
-    yBtn.classList.add('rotate-y-btn');
-    yBtn.classList.add('rotate-btn');
-    yBtn.innerHTML = 'Rotate - Y';
-    container.appendChild(yBtn);
-
-    return container;
-  };
-
-  const createGridContainer = () => {
-    const container = document.createElement('div');
-    container.classList.add('popup-gb-container');
-
-    const gameboard = document.createElement('div');
-    gameboard.classList.add('popup-gb');
-    gameboard.classList.add('gameboard-interface');
-    container.appendChild(gameboard);
-
-    for (let i = 0; i < 10; ++i) {
-      const row = document.createElement('div');
-      row.classList.add('gb-row');
-      row.classList.add(`gb-popup-start-row-${i}`);
-      for (let j = 0; j < 10; ++j) {
-        const square = document.createElement('div');
-        square.classList.add('gb-square');
-        square.classList.add(`gb-popup-start-square-${j}`);
-        row.appendChild(square);
-      }
-      gameboard.appendChild(row);
-    }
-
-    return container;
-  };
-
-  const card = createCard();
-  const textContainer = createTextContainer();
-  const btnContainer = createBtnContainer();
-  const gridContainer = createGridContainer();
-
-  const containerArr = [textContainer, btnContainer, gridContainer];
-  containerArr.forEach((container) => card.appendChild(container));
-  spuGridLogic(gridContainer.firstChild, btnContainer);
-
-  return card;
-}
-
-export { createStartPopup };
+export default spuGridLogic;
